684bfb88653f0de55e0cbed2373f0fc65b64e3c1
Reinaldo Alberto <reinaldoacdc@gmail.com>
2023-03-16 15:07:44 -0300

    feat: progress bar

--- client/islands/FilePicker.tsx
+++ client/islands/FilePicker.tsx
@@ -1,27 +1,57 @@
 import { useState, useRef, useCallback } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
-
+import  Progress from '../components/ProgressBar.tsx'
 
 
 
 export default function FilePicker() {
-    const fileList :string[] = []
+    const [progress, setProgress] = useState(0)
+    const [total, setTotal] = useState(0)
+    const onStart = () => {console.log('start')};
+    const onChange = (_, val) => {console.log('changes')};
+    const onComplete = () => {
+      console.log('complete')
+    }
+
+
+    const fileList :File[] = useState<File[]>([])
+    //const fileList :string[] = []
     const inputField = useRef(null) 
-    const [results, setResults] = useState()
-    
-    const show = (file: string) => {
+
+    const show = (file: File) => {
       fileList.push(file)
     }
     const showFiles = () => {
-      fileList.forEach(el => console.log('file: ', el))
+      console.log('len: ', fileList.length)
+      this.setState({
+        total: fileList.length
+      })
+      fileList.forEach(el => {
+        this.setState({
+          progress: (this.state.progress * 1.3).toFixed(2)
+        });
+        console.log('afile: ', el)})
     }
+    const [results, setResults] = useState<File[]>([])
+
+    const toggleState = useCallback( async () => {
+      let index = 0
+      setTotal(fileList.length)
+      console.log('total: ', total)
+      fileList.forEach( el => {
+        setProgress( index++ )
+        console.log('file: ')
+        console.log('progress: ', progress)
+      })        
+    });
     
 
   return (
+    <div>
     <div class="flex gap-2 w-full">
       
       <Button onClick={() => {
-        showFiles()
+        toggleState()
         // console.log(document.getElementById('ff').value)
         }}>Select folder</Button>
       <input type="file" webkitdirectory 
@@ -35,6 +65,23 @@ export default function FilePicker() {
             }
             class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">  
       </input>
+      <br/>
+      
+    </div>
+    <div>
+      <label for="loader">Downloading progress:</label>
+      
+      <Progress 
+        id="loader" 
+        className="loader"
+        value={ progress } 
+        height="100px" 
+        color="#6cc644"
+        onComplete={ onComplete }
+        onChange={ onChange }
+        onStart={ onStart }
+      />
+      </div>
     </div>
   );
 }
